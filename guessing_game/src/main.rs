98e29a0b7768ac43e38655650a4cfa0de4075667
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-08-23 20:21:10 +0700

    feat: complete guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess:");
+
+        let mut guess = String::new();
+
+        // If `io` is not imported, use:
+        // std::io::stdin()
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // Without `.expect(...)`, the program compiles with an unused `Result` warning.
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
