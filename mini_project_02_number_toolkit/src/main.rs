c18a1223e21b47c70e996882277b8eb89b08e7cf
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-08-28 19:16:00 +0700

    feat: mini_project_02_number_toolkit check number properties

--- mini_project_02_number_toolkit/src/main.rs
+++ mini_project_02_number_toolkit/src/main.rs
@@ -3,7 +3,11 @@ use std::io;
 fn main() {
     println!("Number Toolkit");
     let number = read_positive_integer();
-    println!("{number}");
+    println!("Number: {number}");
+    println!("Even: {}", is_even(number));
+    println!("Prime: {}", is_prime(number));
+    println!("Divisible by 3: {}", is_divisible_3(number));
+    println!("Divisible by 5: {}", is_divisible_5(number));
 }
 
 fn read_positive_integer() -> u32 {
@@ -25,4 +29,29 @@ fn read_positive_integer() -> u32 {
             return number;
         }
     }
+}
+
+fn is_even(number: u32) -> bool {
+    number % 2 == 0
+}
+
+fn is_prime(number: u32) -> bool {
+    if number < 2 {
+        return false;
+    }
+
+    for index in 2..number {
+        if number % index == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_divisible_3(number: u32) -> bool {
+    number % 3 == 0
+}
+
+fn is_divisible_5(number: u32) -> bool {
+    number % 5 == 0
 }
