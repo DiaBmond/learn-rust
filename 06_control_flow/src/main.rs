65ebf509cc8db1da53db5c146be273ca0d077f7e
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-08-28 11:04:59 +0700

    feat: 06_control_flow learn if expressions

--- 06_control_flow/src/main.rs
+++ 06_control_flow/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     if_expression_function(3);
     multiple_conditions(9);
     if_in_statement(true);
+
+    loop_expression_function();
+    loop_in_statement();
+    loop_label_function();
+    while_loop_function();
+    for_loop_function();
 }
 
 fn if_expression_function(number: i16) {
@@ -34,4 +40,81 @@ fn if_in_statement(condition: bool) {
 
     // Error: all branches of an `if` expression must evaluate to the same type.
     // let number = if condition { 5 } else { "six" };
+}
+
+fn loop_expression_function() {
+    let mut count = 0;
+    loop {
+        if count > 5 {
+            println!("count is: {count}");
+            break;
+        }
+        count += 1;
+    }
+}
+
+fn loop_in_statement() {
+    // `loop` is an expression, so its resulting value can be
+    // used as the initializer in a `let` statement.
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn loop_label_function() {
+    let mut count = 0;
+
+    // A loop label can be used to break out of an outer loop.
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn while_loop_function() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
+
+fn for_loop_function() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        // `.rev()` iterates over the range in reverse: 3, 2, 1
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
 }
