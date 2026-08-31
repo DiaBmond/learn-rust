a90a87b0b2a06a32a70b257a59251cd0dee9e5ef
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-08-31 19:19:41 +0700

    feat: 07_ownership learn return values and return mutiple values ownership

--- 07_ownership/src/main.rs
+++ 07_ownership/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    println!("ownership!");
+    println!("***OWNERSHIP***");
     variable_scope();
     string_type();
     copy_vs_move();
     scope_and_assignment();
     clone_string();
     ownership_and_functions();
+    return_values_and_scope();
+    return_multiple_values();
 }
 
 fn variable_scope() {
@@ -97,6 +99,7 @@ fn ownership_and_functions() {
     makes_copy(x); // Because `i32` implements the `Copy` trait,
                    // `x` does NOT move into the function,
                    // so it's okay to use `x` afterward.
+    println!();
 } // Here, `x` goes out of scope, then `s`. Because `s`'s value was moved,
   // nothing needs to be dropped for `s`.
 
@@ -110,3 +113,51 @@ fn makes_copy(some_integer: i32) {
     // `some_integer` comes into scope
     println!("{some_integer}");
 } // Here, `some_integer` goes out of scope. Nothing special happens.
+
+fn return_values_and_scope() {
+    let _s1 = gives_ownership(); // `gives_ownership` moves its return
+                                 // value into `_s1`
+
+    let s2 = String::from("hello"); // `s2` comes into scope
+
+    let _s3 = takes_and_gives_back(s2); // `s2` is moved into
+                                        // `takes_and_gives_back`, which moves
+                                        // its return value into `_s3`
+} // Here, `_s3` goes out of scope and is dropped. `s2` was moved, so nothing
+  // needs to be dropped for `s2`. `_s1` goes out of scope and is dropped.
+
+fn gives_ownership() -> String {
+    // `gives_ownership` moves its return value
+    // to the calling function.
+
+    let some_string = String::from("yours"); // `some_string` comes into scope
+
+    some_string // `some_string` is returned,
+                // moving ownership to the calling function
+}
+
+// This function takes a `String` and returns a `String`.
+fn takes_and_gives_back(a_string: String) -> String {
+    // `a_string` comes into scope
+
+    a_string // `a_string` is returned,
+             // moving ownership to the calling function
+}
+
+fn return_multiple_values() {
+    println!("== Return Multiple Values ==");
+
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1); // returns both values in a tuple
+
+    println!("The length of '{s2}' is {len}.");
+
+    println!();
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // `.len()` returns the length of the String
+
+    (s, length) // returns ownership of `s` together with its length
+}
