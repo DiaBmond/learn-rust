7f62ad4cd282c86358d9d3a97a757e4a07282ea9
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-01 19:03:01 +0700

    feat: 08_references_borrowing learn dangling reference examples

--- 08_references_borrowing/src/main.rs
+++ 08_references_borrowing/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     references_and_borrowing();
     mutable_references();
+    dangling_references();
 }
 
 fn references_and_borrowing() {
@@ -79,4 +80,29 @@ fn mutable_references() {
 
 fn change(some_string: &mut String) {
     some_string.push_str(", world");
+}
+
+fn dangling_references() {
+    println!("== Dangling References ==");
+
+    // Error: missing lifetime specifier
+    // let reference_to_nothing = dangle();
+
+    let reference_to_nothing = no_dangle();
+    println!("{reference_to_nothing}");
+
+    println!();
+}
+
+// A reference cannot outlive the value it refers to.
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+//
+//     &s
+// }
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s
 }
