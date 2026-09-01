ac05e7bdd232f856f8717d7b9b3e419715f76c53
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-01 10:47:37 +0700

    feat: 08_references_borrowing learn mutable references

--- 08_references_borrowing/src/main.rs
+++ 08_references_borrowing/src/main.rs
@@ -1,18 +1,39 @@
 fn main() {
     references_and_borrowing();
+    mutable_references();
 }
 
 fn references_and_borrowing() {
     println!("== References and Borrowing ==");
+
     let s1 = String::from("hello");
 
     let len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
 
-    println!("");
+    println!();
 }
 
 fn calculate_length(s: &String) -> usize {
+    // `&String` is an immutable reference, so the value cannot be changed.
+    // s.push_str(", world"); // Error: cannot borrow as mutable
+
     s.len()
+}
+
+fn mutable_references() {
+    println!("== Mutable References ==");
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    println!("{s}");
+
+    println!();
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
