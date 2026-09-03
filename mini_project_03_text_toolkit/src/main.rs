56f30e6c5c6d43ff70f7f1259c6f0e90d46a1e63
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-03 10:53:03 +0700

    feat: mini_project_03_text_toolkit add text statistics

--- mini_project_03_text_toolkit/src/main.rs
+++ mini_project_03_text_toolkit/src/main.rs
@@ -4,10 +4,18 @@ fn main() {
     println!("== Start Text Toolkit ==");
 
     let text = read_text();
+    let text = text.trim();
 
-    println!("Input is: {}", text.trim());
-    println!("First word is: {}", find_first_word(text.trim()));
-    println!("Last word is: {}", find_last_word(text.trim()));
+    println!("Input is: {text}");
+    println!("First word is: {}", find_first_word(text));
+    println!("Last word is: {}", find_last_word(text));
+
+    let (chars, spaces, words, length) = count_text_stats(text);
+
+    println!("Characters: {chars}");
+    println!("Spaces: {spaces}");
+    println!("Words: {words}");
+    println!("Length: {length}");
 }
 
 fn read_text() -> String {
@@ -44,4 +52,28 @@ fn find_last_word(text: &str) -> &str {
     }
 
     &text[..]
+}
+
+fn count_text_stats(text: &str) -> (u32, u32, u32, u32) {
+    let mut chars = 0;
+    let mut spaces = 0;
+    let mut length = 0;
+
+    let bytes = text.as_bytes();
+
+    for (_i, &item) in bytes.iter().enumerate() {
+        length += 1;
+
+        if item == b' ' {
+            spaces += 1;
+            continue;
+        }
+
+        chars += 1;
+    }
+
+    // Assumes words are separated by a single space.
+    let words = spaces + 1;
+
+    (chars, spaces, words, length)
 }
