c4384a3606a42c573c009241af6dd36b41d884e0
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-03 10:12:53 +0700

    feat: mini_project_03_text_toolkit add first and last word slices

--- mini_project_03_text_toolkit/src/main.rs
+++ mini_project_03_text_toolkit/src/main.rs
@@ -1,18 +1,47 @@
 use std::io;
+
 fn main() {
-    println!("== Start text tookit ==");
+    println!("== Start Text Toolkit ==");
+
     let text = read_text();
-    println!("input is: {text}");
+
+    println!("Input is: {}", text.trim());
+    println!("First word is: {}", find_first_word(text.trim()));
+    println!("Last word is: {}", find_last_word(text.trim()));
 }
 
 fn read_text() -> String {
-    println!("Please enter a text:");
+    println!("Please enter text:");
 
-    let mut read = String::new();
+    let mut text = String::new();
 
     io::stdin()
-        .read_line(&mut read)
+        .read_line(&mut text)
         .expect("Failed to read line");
 
-    read
+    text
+}
+
+fn find_first_word(text: &str) -> &str {
+    let bytes = text.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &text[..i];
+        }
+    }
+
+    &text[..]
+}
+
+fn find_last_word(text: &str) -> &str {
+    let bytes = text.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate().rev() {
+        if item == b' ' {
+            return &text[i + 1..];
+        }
+    }
+
+    &text[..]
 }
