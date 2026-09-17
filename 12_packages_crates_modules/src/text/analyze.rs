f8e9aa30b2f5e15be245cc41f33015e42f82ac02
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-17 11:04:39 +0700

    fix: 12_packages_crates_modules comment warning

--- 12_packages_crates_modules/src/text/analyze.rs
+++ 12_packages_crates_modules/src/text/analyze.rs
@@ -22,52 +22,52 @@ pub fn find_last_word(text: &str) -> &str {
     &text[..]
 }
 
-fn count_text_stats(text: &str) -> (u32, u32, u32, u32) {
-    let mut chars = 0;
-    let mut spaces = 0;
-    let mut length = 0;
+// fn count_text_stats(text: &str) -> (u32, u32, u32, u32) {
+//     let mut chars = 0;
+//     let mut spaces = 0;
+//     let mut length = 0;
 
-    let bytes = text.as_bytes();
+//     let bytes = text.as_bytes();
 
-    for (_i, &item) in bytes.iter().enumerate() {
-        length += 1;
+//     for (_i, &item) in bytes.iter().enumerate() {
+//         length += 1;
 
-        if item == b' ' {
-            spaces += 1;
-            continue;
-        }
+//         if item == b' ' {
+//             spaces += 1;
+//             continue;
+//         }
 
-        chars += 1;
-    }
+//         chars += 1;
+//     }
 
-    // Assumes words are separated by a single space.
-    let words = spaces + 1;
+//     // Assumes words are separated by a single space.
+//     let words = spaces + 1;
 
-    (chars, spaces, words, length)
-}
+//     (chars, spaces, words, length)
+// }
 
 mod edit {
 
-    fn add_word(text: &mut String) {
-        println!("Enter a new word:");
+    // fn add_word(text: &mut String) {
+    //     println!("Enter a new word:");
 
-        let new_word = super::read_text();
+    //     let new_word = super::read_text();
 
-        text.push_str(" ");
-        text.push_str(&new_word);
-        println!("**************");
-        println!();
-    }
+    //     text.push_str(" ");
+    //     text.push_str(&new_word);
+    //     println!("**************");
+    //     println!();
+    // }
 }
 
-use std::io;
+// use std::io;
 
-fn read_text() -> String {
-    let mut text = String::new();
+// fn read_text() -> String {
+//     let mut text = String::new();
 
-    io::stdin()
-        .read_line(&mut text)
-        .expect("Failed to read line");
+//     io::stdin()
+//         .read_line(&mut text)
+//         .expect("Failed to read line");
 
-    text.trim().to_string()
-}
+//     text.trim().to_string()
+// }
