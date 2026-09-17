05d77d11456276c679a139d48979d87c1aa3799c
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-17 10:34:48 +0700

    feat: 12_packages_crates_modules learn module organization

--- 12_packages_crates_modules/src/lib.rs
+++ 12_packages_crates_modules/src/lib.rs
@@ -1,80 +1,4 @@
-mod text {
-    pub mod analyze {
-        pub fn find_first_word(text: &str) -> &str {
-            let bytes = text.as_bytes();
-
-            for (i, &item) in bytes.iter().enumerate() {
-                if item == b' ' {
-                    return &text[..i];
-                }
-            }
-
-            &text[..]
-        }
-
-        pub fn find_last_word(text: &str) -> &str {
-            let bytes = text.as_bytes();
-
-            for (i, &item) in bytes.iter().enumerate().rev() {
-                if item == b' ' {
-                    return &text[i + 1..];
-                }
-            }
-
-            &text[..]
-        }
-
-        fn count_text_stats(text: &str) -> (u32, u32, u32, u32) {
-            let mut chars = 0;
-            let mut spaces = 0;
-            let mut length = 0;
-
-            let bytes = text.as_bytes();
-
-            for (_i, &item) in bytes.iter().enumerate() {
-                length += 1;
-
-                if item == b' ' {
-                    spaces += 1;
-                    continue;
-                }
-
-                chars += 1;
-            }
-
-            // Assumes words are separated by a single space.
-            let words = spaces + 1;
-
-            (chars, spaces, words, length)
-        }
-    }
-
-    mod edit {
-
-        fn add_word(text: &mut String) {
-            println!("Enter a new word:");
-
-            let new_word = super::read_text();
-
-            text.push_str(" ");
-            text.push_str(&new_word);
-            println!("**************");
-            println!();
-        }
-    }
-
-    use std::io;
-
-    fn read_text() -> String {
-        let mut text = String::new();
-
-        io::stdin()
-            .read_line(&mut text)
-            .expect("Failed to read line");
-
-        text.trim().to_string()
-    }
-}
+mod text;
 
 pub fn analyze_text(text: &str) {
     // Absolute path
@@ -82,4 +6,11 @@ pub fn analyze_text(text: &str) {
 
     // Relative path
     text::analyze::find_last_word(text);
+}
+use crate::text::analyze;
+use crate::text::analyze::find_last_word;
+
+pub fn analyze_text_2(text: &str) {
+    analyze::find_first_word(text);
+    find_last_word(text);
 }
---
+++ 12_packages_crates_modules/src/text/analyze.rs
@@ -0,0 +1,73 @@
+pub fn find_first_word(text: &str) -> &str {
+    let bytes = text.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &text[..i];
+        }
+    }
+
+    &text[..]
+}
+
+pub fn find_last_word(text: &str) -> &str {
+    let bytes = text.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate().rev() {
+        if item == b' ' {
+            return &text[i + 1..];
+        }
+    }
+
+    &text[..]
+}
+
+fn count_text_stats(text: &str) -> (u32, u32, u32, u32) {
+    let mut chars = 0;
+    let mut spaces = 0;
+    let mut length = 0;
+
+    let bytes = text.as_bytes();
+
+    for (_i, &item) in bytes.iter().enumerate() {
+        length += 1;
+
+        if item == b' ' {
+            spaces += 1;
+            continue;
+        }
+
+        chars += 1;
+    }
+
+    // Assumes words are separated by a single space.
+    let words = spaces + 1;
+
+    (chars, spaces, words, length)
+}
+
+mod edit {
+
+    fn add_word(text: &mut String) {
+        println!("Enter a new word:");
+
+        let new_word = super::read_text();
+
+        text.push_str(" ");
+        text.push_str(&new_word);
+        println!("**************");
+        println!();
+    }
+}
+
+use std::io;
+
+fn read_text() -> String {
+    let mut text = String::new();
+
+    io::stdin()
+        .read_line(&mut text)
+        .expect("Failed to read line");
+
+    text.trim().to_string()
+}
