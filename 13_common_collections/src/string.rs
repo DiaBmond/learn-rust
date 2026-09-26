ec0b548f795107b5abde6c97d11635aea96d1423
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-09-26 10:50:54 +0700

    refactor: 13_common_collections organize collection modules

--- 13_common_collections/src/string.rs
+++ 13_common_collections/src/string.rs
@@ -1,3 +1,15 @@
+pub enum LearnString {
+    BasicString,
+}
+
+pub fn learn_string(learn: LearnString) -> Option<String> {
+    match learn {
+        LearnString::BasicString => {
+            return basic_string();
+        }
+    }
+}
+
 pub fn basic_string() -> Option<String> {
     let empty = String::new();
 
@@ -15,4 +27,21 @@ pub fn basic_string() -> Option<String> {
     Some(format!(
         "Empty: '{empty}', Variable: '{from_variable}', Literal: '{from_literal}', From: '{from_function}', Thai: '{thai}', Japanese: '{japanese}', Chinese: '{chinese}'"
     ))
+}
+
+#[cfg(test)]
+mod tests_string {
+    use super::*;
+
+    #[test]
+    fn learn_basic_string() {
+        let result = learn_string(LearnString::BasicString);
+
+        assert_eq!(
+            result,
+            Some(String::from(
+                "Empty: '', Variable: 'initial contents', Literal: 'initial contents', From: 'initial contents', Thai: 'สวัสดี', Japanese: 'こんにちは', Chinese: '你好'"
+            ))
+        );
+    }
 }
--- 13_common_collections/src/vector.rs
+++ 13_common_collections/src/vector.rs
@@ -1,3 +1,32 @@
+pub enum LearnVector {
+    BasicVector,
+    ReadingVector,
+    OwnerVector,
+    IteratingVector,
+    EnumVector,
+}
+
+pub fn learn_vector(learn: LearnVector) -> Option<String> {
+    match learn {
+        LearnVector::BasicVector => {
+            let number = 7;
+            return basic_vector(Some(number));
+        }
+        LearnVector::ReadingVector => {
+            return reading_vectors();
+        }
+        LearnVector::OwnerVector => {
+            return owner_with_vector();
+        }
+        LearnVector::IteratingVector => {
+            return iterating_over_vector();
+        }
+        LearnVector::EnumVector => {
+            return enum_with_vector();
+        }
+    }
+}
+
 pub fn basic_vector(number: Option<i32>) -> Option<String> {
     // Basic vector
     let Some(number) = number else {
@@ -115,4 +144,49 @@ pub fn enum_with_vector() -> Option<String> {
     }
 
     Some(text)
+}
+
+#[cfg(test)]
+mod tests_vector {
+    use super::*;
+
+    #[test]
+    fn learn_basic_vector() {
+        let result = learn_vector(LearnVector::BasicVector);
+
+        assert_eq!(result, Some(String::from("The first element is 7")));
+    }
+
+    #[test]
+    fn learn_reading_vector() {
+        let result = learn_vector(LearnVector::ReadingVector);
+
+        assert_eq!(result, Some(String::from("There is no hundredth element.")));
+    }
+
+    #[test]
+    fn learn_owner_vector() {
+        let result = learn_vector(LearnVector::OwnerVector);
+
+        assert_eq!(result, Some(String::from("Vector length is 6")));
+    }
+
+    #[test]
+    fn learn_iterating_over_vector() {
+        let result = learn_vector(LearnVector::IteratingVector);
+
+        assert_eq!(
+            result,
+            Some(String::from(
+                "Values in v1: [100, 32, 57] Values in v2: [150, 82, 107]"
+            ))
+        );
+    }
+
+    #[test]
+    fn learn_enum_with_vector() {
+        let result = learn_vector(LearnVector::EnumVector);
+
+        assert_eq!(result, Some(String::from("Int:3 Text:blue Float:10.12 ")));
+    }
 }
